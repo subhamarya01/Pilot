94b8ba94975f7ba191996913fc0a0c5e7cb237b4
subhamarya01 <subham.sap@gmail.com>
2026-05-25 14:22:50 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AdoptionIQ Phase 1",
+    page_icon="📊",
+    layout="wide"
+)
+
+st.title("📊 AdoptionIQ Phase 1")
+st.write("Excel Reader + KPI Engine + Dashboard")
+
+st.success("App is running successfully!")
